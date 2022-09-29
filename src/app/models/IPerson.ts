b3e20afe63019018917136373344ad70db6effee
nickbodaveli <nickbodaveli@gmail.com>
2022-09-29 17:07:23 +0400

    display main page and view

--- src/app/models/IPerson.ts
+++ src/app/models/IPerson.ts
@@ -2,4 +2,17 @@ export interface IPerson {
     id: string;
     usersId: string;
     name: string;
+    experiences : IExperiences;
+}
+
+export interface IExperiences {
+    id: string;
+    personsId: string;
+    workingExperiences: Array<IWorkingExperiences>
+}
+
+export interface IWorkingExperiences {
+    id: string;
+    experiencesId: string;
+    name:string;
 }
